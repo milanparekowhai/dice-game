a75d14e1d2befe13509fbdc0217ca9b4100c7559
milanparekowhai <150859838+milanparekowhai@users.noreply.github.com>
2024-02-22 10:41:30 +1300

    Add files via upload
    randem intagers

--- C_02_yes.no.py
+++ C_02_yes.no.py
@@ -1,9 +1,9 @@
 # checks users enter yes (y) or no (n)
-def yes_no(question):
+def yes_no(question, responce=None):
     while True:
         want_instructions = input("do you want to read instructions ").lower()
 
-
+        from urllib import response
         if want_instructions == "yes" or response == "y":
             return "yes"
         elif response == "no" or responce == "n":
@@ -12,8 +12,8 @@ def yes_no(question):
 
 # Main route
 while True:
-want_insrtuctions = yes_no("do you want to read the instructions")
-print(f"You chose {want_insrtuctions} ")
+    want_insrtuctions = yes_no("do you want to read the instructions")
+    print(f"You chose {want_insrtuctions} ")
 
 roll_again = yes_no("do you want to roll again ")
 print(f'you said {roll_again} to roll again. ')
