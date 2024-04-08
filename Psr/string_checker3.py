44ca6b2a390b91e4118abeff44477d1615c63458
milanparekowhai <150859838+milanparekowhai@users.noreply.github.com>
2024-04-09 10:21:53 +1200

    Add files via upload

---
+++ Psr/string_checker3.py
@@ -0,0 +1,34 @@
+# Check that users have entered a valid
+# option based on a list
+def string_checker(question, valid_ans):
+    while True:
+
+        # Get user response and make sure its lowercase
+        user_response = input(question).lower()
+
+        for item in valid_ans:
+            # check if the user response is a word in the lst
+            if item == user_response:
+                return item
+
+            # check if the user response is the same as
+            # the first letter of a item in the list.
+
+            elif user_response == item[0]:
+
+                return item
+
+        return "invalid"
+
+
+# main route goes here
+yes_no = ["yes", "no"]
+rps_list = ["rock", "paper", "scissors", "xxx"]
+
+
+want_instuctions = string_checker("Do you want to see the instructions? "),(yes_no)
+
+
+
+
+print("Too chose", want_instuctions)
